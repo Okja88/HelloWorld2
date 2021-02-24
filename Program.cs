01d889a5dbac8a27a5398d9f3f19666ad8978e67
Okja88 <kerhl8@gmail.com>
2021-02-24 14:48:10 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am Nathan and this coding is cool!");
+            Console.WriteLine("hi!");
+            Console.WriteLine("This is awesome!");
+        }
+    }
+}
