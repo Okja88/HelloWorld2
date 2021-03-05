ab1e1d794d87f9c42534a1f300ab25ae9c387c79
Okja88 <79552371+Okja88@users.noreply.github.com>
2021-03-05 10:05:28 +0800

    Age Calculator new update

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 
-namespace HelloWorld2
+namespace AgeCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I am Nathan and this coding is cool!");
-            Console.WriteLine("hi!");
-            Console.WriteLine("This is awesome!");
-        }
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(" Enter your name here: ");  
+                string name = Console.ReadLine();
+
+            Console.WriteLine(" Please enter your age: ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine($"hello {name}! \nWelcome to C# Essential. \n Your age is {age}.");
+
+            Console.WriteLine($"And your next age is {double.Parse(age) + 1}."); //convert age to integer by adding double.Parse(age), 
+            //note age is bracketed. if parsed to int, might not work. hence parsed as double, works w decimal points
+            
+         }
     }
 }
